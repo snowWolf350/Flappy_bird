2402c63599ff16389eacaebab7a126685ebb87f7
snowWolf350 <pranet.v08@gmail.com>
2026-05-01 21:31:45 +0530

    added sprites

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public string _playerName;
+    public int _playerSpeed;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
