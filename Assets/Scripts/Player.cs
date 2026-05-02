d20bc3255086db49ab3fbce22559f833f23159c2
snowWolf350 <pranet.v08@gmail.com>
2026-05-02 08:05:08 +0530

    added flapping

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,18 +2,21 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public string _playerName;
-    public int _playerSpeed;
+    public Rigidbody2D _playerRigidBody;
+    public float _flapStrength = 0.5f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _playerRigidBody.linearVelocity += Vector2.up * _flapStrength;  
+        }
     }
 }
